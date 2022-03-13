82553e3be5243f4377b31c99941c474c5fad7987
Kacper Hemperek <kacper.hemperek@o2.pl>
2022-03-13 20:37:43 +0100

    making redux state not returning null after page reload with redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,8 +3,19 @@ import logger from "redux-logger";
 
 import rootReducer from "./root.reducer";
 
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  stateReconciler: autoMergeLevel2,
+};
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
-export default store;
+export const store = createStore(pReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
